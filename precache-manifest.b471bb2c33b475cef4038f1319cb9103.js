887df7da2e83d9bc1591e82e718f3814813ea909
tamsay <longetemitope@gmail.com>
2020-03-27 02:44:27 +0100

    First build of the project

---
+++ precache-manifest.b471bb2c33b475cef4038f1319cb9103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0962230f5fe52957905129b987e0e9c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae046a244955d619406a",
+    "url": "/static/css/main.b80a84f8.chunk.css"
+  },
+  {
+    "revision": "d65cd17bf9a22e02b90a",
+    "url": "/static/js/2.5ebca8c6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5ebca8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae046a244955d619406a",
+    "url": "/static/js/main.aaa61de6.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
